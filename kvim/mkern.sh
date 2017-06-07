065edd25b54ddd953d953d9cf3e65d809cf1c6b6
terry <terry@szwesion.com>
2017-06-07 14:32:42 +0800

    fix mkern.sh can not compile error

--- kvim/mkern.sh
+++ kvim/mkern.sh
@@ -17,21 +17,22 @@ KERNEL_OUT=out/target/product/kvim/obj/KERNEL_OBJ
 if [ ! -f $KERNEL_OUT/.config ]; then
     make -C common O=../$KERNEL_OUT kvim_defconfig ARCH=arm64 CROSS_COMPILE=$PREFIX_CROSS_COMPILE
 fi
-if [ "$2" != "m" ]; then
-    make -C common O=../$KERNEL_OUT ARCH=arm64 -j6 CROSS_COMPILE=$PREFIX_CROSS_COMPILE UIMAGE_LOADADDR=0x1008000
-fi
-make -C common O=../$KERNEL_OUT modules ARCH=arm64 -j6 CROSS_COMPILE=$PREFIX_CROSS_COMPILE
+#if [ "$2" != "m" ]; then
+#    make -C common O=../$KERNEL_OUT ARCH=arm64 -j6 CROSS_COMPILE=$PREFIX_CROSS_COMPILE UIMAGE_LOADADDR=0x1008000
+#fi
+make -C common O=../$KERNEL_OUT ARCH=arm64 -j6 CROSS_COMPILE=$PREFIX_CROSS_COMPILE modules Image.gz
 
 if [ "$2" != "m" ]; then
 #    make -C common O=../$KERNEL_OUT kvim.dtd ARCH=arm64 CROSS_COMPILE=$PREFIX_CROSS_COMPILE
-    make -C common O=../$KERNEL_OUT kvim.dtb ARCH=arm64 CROSS_COMPILE=$PREFIX_CROSS_COMPILE
+    make -C common O=../$KERNEL_OUT kvim.dtb ARCH=arm64 CROSS_COMPILE=$PREFIX_CROSS_COMPILE PARTITION_DTSI=partition_mbox.dtsi
 fi
 
 
 if [ "$2" != "m" ]; then
-    out/host/linux-x86/bin/mkbootimg --kernel common/../$KERNEL_OUT/arch/arm64/boot/Image \
+    out/host/linux-x86/bin/mkbootimg --kernel common/../$KERNEL_OUT/arch/arm64/boot/Image.gz \
+        --base 0x0 \
+        --kernel_offset 0x1080000 \
         --ramdisk ${ROOTFS} \
-        --second common/../$KERNEL_OUT/arch/arm64/boot/dts/kvim.dtb \
         --output ./out/target/product/kvim/boot.img
     ls -l ./out/target/product/kvim/boot.img
     echo "boot.img done"
