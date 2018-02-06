089a0da629176dd5b34344152aa3136ccdf2e221
terry <terry@szwesion.com>
2018-02-06 09:37:37 +0800

    Project: remove 32bit build configs

--- kvim2/vendorsetup.sh
+++ kvim2/vendorsetup.sh
@@ -1,6 +1,3 @@
-add_lunch_combo kvim2-eng-32
-add_lunch_combo kvim2-user-32
-add_lunch_combo kvim2-userdebug-32
 add_lunch_combo kvim2-eng-64
 add_lunch_combo kvim2-user-64
 add_lunch_combo kvim2-userdebug-64
