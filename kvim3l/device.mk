5a0642cb32a25a8223b1388a38f76524d34b8716
jason <jason@khadas.com>
2022-04-19 15:53:32 +0000

    light: vim3l: add light.amlogic HAL support
    Signed-off-by: jason <jason@khadas.com>
    Change-Id: I5614500bbfd72838c81a781c0cb9d7ed2b83a59c

--- kvim3l/device.mk
+++ kvim3l/device.mk
@@ -116,6 +116,11 @@ PRODUCT_COPY_FILES += \
     device/khadas/common/ril/libquectel-ril/ip-down:system/etc/ppp/ip-down \
     device/khadas/common/ril/apns-conf.xml:system/etc/apns-conf.xml \
     device/khadas/common/ril/ql-ril.conf:system/etc/ql-ril.conf
+
+# Light HAL
+PRODUCT_PACKAGES += \
+    lights.amlogic
+
 # GPS HAL
 PRODUCT_PACKAGES += \
     gps.amlogic
