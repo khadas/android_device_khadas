fc414371ca573e664c25aa8e2efbfc27defdd528
Xiong Zhang <xiong.zhang@wesion.com>
2023-11-06 17:06:15 +0800

    Fix ro.build.display.id attribute and product information
    Signed-off-by: Xiong Zhang <xiong.zhang@wesion.com>

--- kvim3/kvim3.mk
+++ kvim3/kvim3.mk
@@ -84,11 +84,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.hdmi.set_menu_language=false \
     persist.sys.hdmi.keep_awake=false
 
-PRODUCT_NAME := $(TARGET_PRODUCT)
-PRODUCT_DEVICE := $(TARGET_PRODUCT)
-PRODUCT_BRAND := Amlogic
-PRODUCT_MODEL := $(TARGET_PRODUCT)
-PRODUCT_MANUFACTURER := Amlogic
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.display.id = VIM3-Android-11-32bit-V$(shell date +%y%m%d)
+
+PRODUCT_NAME := kvim3
+PRODUCT_DEVICE := kvim3
+PRODUCT_BRAND := Khadas
+PRODUCT_MODEL := VIM3
+PRODUCT_MANUFACTURER := Khadas
 
 PRODUCT_TYPE := mbox
 
