704034e5c753486146c78f41ab840ce697b55ca4
Xiong Zhang <xiong.zhang@wesion.com>
2023-11-09 11:30:40 +0800

    device: move ctc compile reules to common/media.mk [1/4]
    Signed-off-by: Xiong Zhang <xiong.zhang@wesion.com>

--- common/media.mk
+++ common/media.mk
@@ -55,6 +55,7 @@ TARGET_WITH_MEDIA_EXT_LEVEL := 4
 #set on some prducts,used libplayer.
 BUILD_WITH_BOOT_PLAYER :=true
 BUILD_WITH_ES_PLAYER := true
+BUILD_WITH_CTC_MEDIAPROCESSOR := false
 BUILD_WITH_DEC_INFO_TEST := true
 BUILD_WITH_AML_MP ?= false
 BUILD_SYSTEM_AML_MP ?= true
@@ -436,3 +437,21 @@ BOARD_AML_MEDIA_HAL_CONFIG := $(BOARD_AML_MEDIAHAL_PATH)/media_base_config.mk
 PRODUCT_COPY_FILES += \
 	device/khadas/common/initscripts/init.amlogic.media.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.media.rc
 
+#########################################################################
+#
+#                     CTC_MediaProcessor
+#
+#########################################################################
+ifeq ($(BUILD_WITH_CTC_MEDIAPROCESSOR),true)
+PRODUCT_PACKAGES += \
+    libffmpeg40 \
+    libFFExtractor \
+    libamFFExtractor \
+    libCTC_MediaProcessor \
+    libCTC_MediaProcessorjni \
+    libCTC_AmlPlayer \
+    mediaProcessorDemo \
+    libminiframework \
+    libliveplayer \
+    libAmIptvMedia
+endif
