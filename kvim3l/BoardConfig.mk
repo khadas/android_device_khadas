659a413583b2d89ba4bb2468de6a217c045a44d7
Xiong Zhang <xiong.zhang@wesion.com>
2023-11-09 15:21:57 +0800

    lcd: [S905D3][BDS][panel][Wesion-MIPI]: 0831 code display 1280x720 mipi->vga not full screen[2/3]
    Signed-off-by: Xiong Zhang <xiong.zhang@wesion.com>

--- kvim3l/BoardConfig.mk
+++ kvim3l/BoardConfig.mk
@@ -60,6 +60,7 @@ USE_HWC2 := true
 #panel does not support AFBC in default
 HWC_PRIMARY_DISP_SUPPORT_AFBC := false
 HWC_EXTEND_DISP_SUPPORT_AFBC := true
+HWC_ENABLE_PRE_DISPLAY_CALIBRATE := true
 
 include hardware/amlogic/gralloc/gralloc.device.mk
 
